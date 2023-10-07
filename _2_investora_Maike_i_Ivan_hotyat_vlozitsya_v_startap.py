ba6e3db876f261a42d7f89b4588667ca53fdff42
Aikaz1988 <135123851+Aikaz1988@users.noreply.github.com>
2023-10-07 20:32:02 +0300

    Add files via upload

---
+++ _2_investora_Maike_i_Ivan_hotyat_vlozitsya_v_startap.py
@@ -0,0 +1,14 @@
+﻿
+x = int(input("Минимальная сумма инвестиции: "))
+a = int(input("Cумма инвестиции Майкла: ")) # Mike
+b = int(input("Cумма инвестиции Ивана: ")) # Ivan
+if (a >= x) and (b >= x):
+    print(2)
+elif (a >= x):
+    print("Mike")
+elif (b >= x):
+    print("Ivan")
+elif(a + b >= x):
+    print(1)
+else:
+    print(0)
