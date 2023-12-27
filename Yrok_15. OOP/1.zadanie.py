33688db71624e92b7329976341dc1bcd64a5dd47
Aikaz1988 <135123851+Aikaz1988@users.noreply.github.com>
2023-12-27 23:08:39 +0300

    Update 1.zadanie.py

--- Yrok_15. OOP/1.zadanie.py
+++ Yrok_15. OOP/1.zadanie.py
@@ -1,31 +1,25 @@
-# Задание №2
-# Создайте класс Autobus, который наследуется от класса Transport. 
-# Дайте аргументу Autobus.seating_capacity() значение по умолчанию 50.
-# Используйте переопределение метода.
-# Используйте следующий код для родительского класса транспортного средства:
+# Задание №1
+# Есть родительский класс:
 # class Transport:
 #    def __init__(self, name, max_speed, mileage):
 #        self.name = name
 #        self.max_speed = max_speed
 #        self.mileage = mileage
-
-#    def seating_capacity(self, capacity):
-#        return f"Вместимость одного автобуса {self.name}  {capacity} пассажиров"
+# Создайте объект Autobus, который унаследует все переменные и методы родительского класса Transport и выведете его.
 # Ожидаемый результат вывода:
-# Вместимость одного автобуса Renaul Logan: 50 пассажиров
+# Название автомобиля: Renaul Logan Скорость: 180 Пробег: 12
 
 class Transport:
-   def __init__(self, name, max_speed, mileage):
-       self.name = name
-       self.max_speed = max_speed
-       self.mileage = mileage
-
-   def seating_capacity(self, capacity):
-       return f"Вместимость одного автобуса {self.name}: {capacity} пассажиров"
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
 
 class Autobus(Transport):
-   def seating_capacity(self, capacity=50):
-       return super().seating_capacity(capacity)
+    pass  # Реализация класса не требуется, потому что нужны все переменные и методы родительского класса
 
-auto1 = Autobus("Renaul Logan", 180, 12000)
-print(auto1.seating_capacity())
+# Создание экземпляра класса Autobus
+auto = Autobus('Renaul Logan', 180, 12)
+
+# Выводим на экран информацию об автобусе
+print(f"Название автомобиля: {auto.name} Скорость: {auto.max_speed} Пробег: {auto.mileage}")
